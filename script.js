65a23807117555b101dce5b8e3a4aa771e27eeae
Sumanththumma <tsumanth3@gmail.com>
2025-01-12 21:19:18 +0530

    added socials

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const API_KEY = 'c8c79e6924b9ad12c2436fec';
-let base='USD';
+let base='INR';
 let URL = `https://v6.exchangerate-api.com/v6/${API_KEY}/latest/${base}`;
 const baseCurrency = document.getElementById('base-currency');
 const convertedCurrency = document.getElementById('converted-currency');
