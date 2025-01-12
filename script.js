e92993c6f27ce88a6aeec97dd7d59319bf345670
Sumanththumma <tsumanth3@gmail.com>
2025-01-12 16:24:15 +0530

    fetching from ExchangeAPI/ collection

--- script.js
+++ script.js
@@ -2,9 +2,28 @@ const API_KEY = 'c8c79e6924b9ad12c2436fec';
 let base='USD';
 let URL = `https://v6.exchangerate-api.com/v6/${API_KEY}/latest/${base}`;
 const baseCurrency = document.getElementById('base-currency');
+const convertedCurrency = document.getElementById('converted-currency');
 
-baseCurrency.addEventListener('change',()=>{
+
+const fetchCurrencies = fetch(`${URL}`).then((Response)=>Response.json()).then((data)=>{
     
-})
+    let clutter = Object.keys(data.conversion_rates);
+    clutter.forEach((curr)=>{
+        let newOption = document.createElement('option');
+        newOption.value = curr;
+        newOption.innerText = curr;
+        baseCurrency.appendChild(newOption);
+   
+    })
+    clutter.forEach((curr)=>{
+        let newOption = document.createElement('option');
+        newOption.value = curr;
+        newOption.innerText = curr;
+        convertedCurrency.appendChild(newOption);
+    })
 
-const fetchCurrencies = fetch('')
+    baseCurrency.addEventListener('change',()=>{
+        
+    })
+
+})
